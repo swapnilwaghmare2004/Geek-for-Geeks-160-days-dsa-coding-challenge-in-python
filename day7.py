8eaea97ce6b23e2b4cb2e84116865303c08ab3ee
swapnilwaghmare2004 <waghmareswapnil563@gmail.com>
2024-12-10 22:37:15 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,50 @@
+Stock Buy and Sell – Multiple Transaction Allowed
+The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+Examples:
+
+Input: prices[] = [100, 180, 260, 310, 40, 535, 695]
+Output: 865
+Explanation: Buy the stock on day 0 and sell it on day 3 => 310 – 100 = 210. Buy the stock on day 4 and sell it on day 6 => 695 – 40 = 655. Maximum Profit = 210 + 655 = 865.
+
+
+Input: prices[] = [4, 2, 2, 2, 4]
+Output: 2
+Explanation: Buy the stock on day 3 and sell it on day 4 => 4 – 2 = 2. Maximum Profit = 2.
+Constraints:
+1 <= prices.size() <= 105
+0 <= prices[i] <= 104
+
+SOLUTION:-
+
+from typing import List
+
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        # code here
+        max_profit = 0
+        n = len(prices)
+        
+        for i in range(n - 1):
+            if prices[i] < prices[i + 1]:
+                max_profit += prices[i + 1] - prices[i]
+        
+        return max_profit
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        solution = Solution()
+        res = solution.maximumProfit(arr)
+        print(res)
+
+# } Driver Code Ends
